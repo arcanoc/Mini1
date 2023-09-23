050871228de028860be3299c14c29e2acb7c1d95
Cha Arcaño <143869263+arcanoc@users.noreply.github.com>
2023-09-23 10:50:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const header = document.querySelector("header");
-
 window.addEventListener ("scroll", function() {
-header.classList.toggle ("sticky", window.scrollY > 100)
+    header.classList.toggle ("sticky", window.scrollY > 200)
 });
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
 
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
